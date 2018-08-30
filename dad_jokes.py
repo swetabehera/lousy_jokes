1e12c149c3184dc015466e1f7806c0550843946f
Sweta Behera <34244004+swetax@users.noreply.github.com>
2018-08-30 23:27:34 +0530

    Update dad_jokes.py

--- dad_jokes.py
+++ dad_jokes.py
@@ -7,7 +7,7 @@ header = pyfiglet.figlet_format("Dad Joke 3000")
 header = termcolor.colored(header, color="magenta")
 print(header)
 
-term = input("Let me tell you a joke! Give me a topic: ")
+term = input("Let me tell you a joke! First give me a topic: ")
 response_json = requests.get(
     "https://icanhazdadjoke.com/search",
     headers={"Accept": "application/json"},
@@ -17,13 +17,13 @@ results = response_json["results"]
 total_jokes = response_json["total_jokes"]
 if total_jokes > 1:
     print(
-        f"I've got {total_jokes} jokes about {term}. Here's one:\n",
+        f"I've got {total_jokes} jokes about your chosen topic : {term}. Here's one:\n",
         choice(results)['joke']
     )
 elif total_jokes == 1:
     print(
-        f"I've got one joke about {term}. Here it is:\n",
+        f"I've got one joke about your chosen topic : {term}. Here it is:\n",
         results[0]['joke']
     )
 else:
-    print(f"Sorry, I don't have any jokes about {term}! Please try again.")
+    print(f"Sorry, I don't have any jokes about your chosen topic : {term}! Please try again.")
